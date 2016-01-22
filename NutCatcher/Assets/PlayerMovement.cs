1e85b2a5ecc11f74f1a2b0e6f7b5164ff180d86c
unknown <1402318@MC8152.uad.ac.uk>
2016-01-22 11:10:40 +0000

    This was made 22nd January 2016

---
+++ NutCatcher/Assets/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+    public float speed = 5;
+    public float x, y;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Move();
+	}
+
+    private void Move()
+    {
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+
+        GetComponent<Rigidbody>().velocity = new Vector2(x * speed, y * speed);
+    }
+}
