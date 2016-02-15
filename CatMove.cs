f90e0671222289aea4125110a931d4cbcc24f4e9
unknown <1402318@MC8152.uad.ac.uk>
2016-02-15 14:15:06 +0000

    Script for allowing the cat to hop onto the swing. The CatMove Script contains my changes that allow the HopOn script to work. (Hopefully, shouldn't be hard to find)

---
+++ CatMove.cs
@@ -0,0 +1,166 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CatMove : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public float jumpSpeed;
+	public float position;
+    public float rotateSpeed;
+    public AudioClip sound;
+    public AudioClip walkingSound;
+    public AudioClip jumpSound;
+    CharacterController controller;
+    AudioSource audio;
+
+    public float gravity;// = 9.8f;
+    protected bool jump;
+    protected bool doublejump;
+    Vector3 currentmovement;
+
+    float MaxHealth;
+    float currHealth;
+    public Slider healthBarSlider;  //reference for slider
+    float soundTimer;
+
+    public Transform respawnPoint;
+	public Transform sBase;
+    // Use this for initialization
+
+    void Start()
+    {
+        //moveSpeed = 3.0f; // J.G. Changed from 8 to 3 for testing // this value is not actually used anywhere. 
+        jumpSpeed = 5.0f;
+        rotateSpeed = 160;
+        soundTimer = 0;
+        controller = GetComponent<CharacterController>();
+        audio = GetComponent<AudioSource>();
+        jump = false;
+        doublejump = false;
+        if (respawnPoint == null)
+        {
+            Debug.Log("Cat does not have a spawn point");
+            respawnPoint = GameObject.FindGameObjectWithTag("SpawnPoint").transform;
+        }
+        ResetPosition();
+	/*	if (sBase == null)
+		{
+			Debug.Log("Cat does not have a spawn point");
+			sBase = GameObject.FindGameObjectWithTag("Swing").transform;
+		}
+		Relocate();*/
+    }
+    public void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "insulin")
+        {
+            healthBarSlider.value += 0.8f;
+            audio.PlayOneShot(sound);
+
+            Destroy(other.gameObject);
+        }
+
+        else if (other.gameObject.tag == "Food")
+        {
+            healthBarSlider.value -= 0.3f;
+            Destroy(other.gameObject);
+        }
+
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Trigger")
+        {
+			GetComponent<Trajectory>().ActivateFlyby();
+		//	moveSpeed = 0;
+          //moveSpeed = GameObject.FindGameObjectWithTag("Swing").GetComponent<CatMove>().position;
+            Debug.Log("I am sitting on a Swing.");
+		/*	if(Input.GetMouseButtonDown(0))
+			{
+				currentmovement.y = jumpSpeed * 5;
+				moveSpeed = 1;
+				Debug.Log("You Can Fly!");
+			}*/
+        }
+        /*The above if check is ran when the object(cat) triggers with the seesaw trigger on a StartPathNode object
+         * This stops the cat moving so that it can travel along the curved path. The next action occurs on RockController 
+         There is also another trigger check on the see saw object which checks for the player.. see SeeSawController*/
+        
+        if (other.gameObject.tag == "EndNodePoint") {
+			GetComponent<Trajectory>().DeactivateFlyby();
+			moveSpeed = 1; //this should be changed
+			Debug.Log ("Let's Go!");
+
+		} 
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        currentmovement.z = moveSpeed;
+        currentmovement = new Vector3(currentmovement.x, currentmovement.y, currentmovement.z);
+        //healthBarSlider.value -= .00011f;
+
+        if (!controller.isGrounded)
+        {
+
+            currentmovement -= new Vector3(0, gravity * Time.deltaTime * 2, 0);
+        }
+        else
+            currentmovement.y = 0;
+
+        if (Input.GetKeyDown(KeyCode.Space)) 	//jump
+        {
+            if (controller.isGrounded)
+            {
+                audio.PlayOneShot(jumpSound);
+               // healthBarSlider.value -= .011f;
+                currentmovement.y = jumpSpeed;
+
+                doublejump = true;
+
+            }
+            else
+            {
+                if (doublejump)
+                {
+                    doublejump = false;
+                    currentmovement.y = jumpSpeed;
+                    audio.PlayOneShot(jumpSound);
+                }
+            }
+        }
+        controller.Move(currentmovement * Time.deltaTime);
+        
+            if (controller.isGrounded)
+            {
+                soundTimer++;
+                if (soundTimer == 10)
+                {
+                    audio.PlayOneShot(walkingSound);
+                    soundTimer = 0;
+                }
+            }
+    }
+
+    public void ResetPosition()
+    {
+        
+        if (respawnPoint != null)
+        {
+            Debug.Log("position of current spawnpoint" + respawnPoint.transform.position);
+            this.gameObject.transform.position = respawnPoint.transform.position;
+        }
+    }
+	public void Relocate()
+	{
+		if (sBase != null)
+		{
+			this.gameObject.transform.position = sBase.transform.position;
+			Debug.Log ("Freeze!");
+		}
+	}
+}
+
---
+++ HopOn.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HopOn : MonoBehaviour {
+
+	public float time;
+	public Transform ConnectBase;
+	
+	void Start()
+	{
+		time = 0;
+		if(ConnectBase == null)
+		{
+			Debug.Log("I can't do anything without an assignment"); //checks if the node has not been assigned as this will cause an error later in code.
+		}
+	}
+	
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			other.gameObject.GetComponent<CatMove>().sBase = ConnectBase;
+		}
+	}
+	
+	void OnTriggerStay(Collider other)
+	{
+		time += 1 * Time.deltaTime; //make sure if making a timer like this that you use time.delta as this is dependant on the devices frame rate
+		if (other.tag == "Player" && time >=0) {
+			GameObject.FindGameObjectWithTag("Player").GetComponent<CatMove>().Relocate(); //This calls Reelocate() function which will change the Players position in world space to the swing base
+			time = 0;
+		}
+	}
+}
